61cc913e851f887b1b45c56e1154550021430588
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-18 11:50:27 +0500

    Fix: nixosModules.module and nixosModules.default now point to module.nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Custom uzbek keyboard layout with support for typographic letters";
 
   outputs = {self}: {
-    nixosModules.default = import ./default.nix;
+    nixosModules.module = import ./module.nix;
+    nixosModules.default = import ./module.nix;
   };
 }
